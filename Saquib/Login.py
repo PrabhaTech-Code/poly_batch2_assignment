e634604629ecaacc2c968714dd062b4985341e03
prabha-technology-private-limted <prabha.technology.01@gmail.com>
2026-06-11 15:52:00 +0530

    added login service

---
+++ Saquib/Login.py
@@ -0,0 +1,7 @@
+username ="saquib@gmail.com"
+password = "saquib123"
+
+if username == "saquib@gmail.com" and password == "saquib123":
+    print("Login successful")
+else:
+    print("Login failed")
