a8ec6c79d7cc3a97bafe4263db464b5838c85239
prabha-technology-private-limted <prabha.technology.01@gmail.com>
2026-06-11 15:59:06 +0530

    added Add & Display Employee code

---
+++ Display_Employee.py
@@ -0,0 +1,10 @@
+Employee = {"Name": "John Doe", 
+            "ID": "E001", 
+            "Department": "IT", 
+            "Position": "Software Engineer", 
+            "Salary": 60000
+            }
+
+print("Employee Details:")
+for key, value in Employee.items():
+    print(f"{key}: {value}")    
